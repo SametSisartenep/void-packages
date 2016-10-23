d7eb6a6c59fbac39b986129786a1dff3fbab15b4
SametSisartenep <rodrigosloop@gmail.com>
2016-10-23 12:32:31 +0200

    dwm & slstatus custom setup.

--- srcpkgs/dwm/files/config.h
+++ srcpkgs/dwm/files/config.h
@@ -2,9 +2,10 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Inconsolata for Powerline:size=12.5",
+	"Material Icons:size=12.5:antialias=false:hinting=false",
 };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "PxPlus IBM VGA8:size=12.5";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
@@ -16,8 +17,32 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
+#define icon_dashboard "\ue871"
+#define icon_view_quilt "\ue8f1"
+#define icon_view_carousel "\ue8eb"
+
+#define icon_web "\ue87a"
+#define icon_sys "\ue85c"
+#define icon_dev "\ue1b0"
+#define icon_mon "\ue2bd"
+#define icon_vm "\ue1bd"
+#define icon_files "\ue880"
+#define icon_irc "\ue0b7"
+#define icon_social "\ue7fb"
+#define icon_aux "\ue859"
+
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {
+	icon_web,
+	icon_sys,
+	icon_dev,
+	icon_mon,
+	icon_vm,
+	icon_files,
+	icon_irc,
+	icon_social,
+	icon_aux,
+};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,6 +52,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Vlc",      NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -36,13 +62,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ icon_view_quilt,     tile },    /* first entry is default */
+	{ icon_dashboard,      NULL },    /* no layout function means floating behavior */
+	{ icon_view_carousel,  monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -55,7 +81,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-f", "Inconsolata for Powerline:size=12.5", NULL };
+static const char *lockscr[]  = { "xautolock", "-locknow", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -64,13 +91,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_k,      killclient,     {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockscr } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
--- srcpkgs/slstatus/files/config.h
+++ srcpkgs/slstatus/files/config.h
@@ -1,18 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* alsa sound */
-#define ALSA_CHANNEL    "Master"
+static const char channel[]   = "Master";
 
 /* battery */
-#define BATTERY_PATH    "/sys/class/power_supply/"
-#define BATTERY_NOW     "energy_now"
-#define BATTERY_FULL    "energy_full_design"
+static const char batterypath[] = "/sys/class/power_supply/";
+static const char batterynow[]  = "charge_now";
+static const char batteryfull[] = "charge_full_design";
 
-/* how often to update the statusbar (min value == 1) */
-#define UPDATE_INTERVAL 1
+/* bar update interval in seconds (smallest value = 1) */
+static unsigned int update_interval = 1;
 
-/* text to show if no value can be retrieved */
-#define UNKNOWN_STR     "n/a"
+/* icons */
+#define icon_wln "\ue1ba"
+#define icon_cpu "\ue1af"
+#define icon_mem "\ue322"
+#define icon_ent "\ue32a"
+#define icon_bat "\ue1a5"
+#define icon_vol "\ue050"
 
 /* statusbar
 - battery_perc (battery percentage) [argument: battery name]
@@ -26,7 +31,6 @@
 - gid (gid of current user) [argument: NULL]
 - hostname [argument: NULL]
 - ip (ip address) [argument: interface]
-- load_avg (load average) [argument: NULL]
 - ram_free (ram usage in percent) [argument: NULL]
 - ram_perc (ram usage in percent) [argument: NULL]
 - ram_total (ram usage in percent) [argument: NULL]
@@ -34,14 +38,18 @@
 - run_command (run custom shell command) [argument: command]
 - temp (temperature in degrees) [argument: temperature file]
 - uid (uid of current user) [argument: NULL]
-- uptime (uptime) [argument: NULL]
 - username (username of current user) [argument: NULL]
 - vol_perc (alsa volume and mute status in percent) [argument: soundcard]
 - wifi_perc (wifi signal in percent) [argument: wifi card interface name]
 - wifi_essid (wifi essid) [argument: wifi card interface name] */
 static const struct arg args[] = {
-	/* function	format			argument */
-	{ cpu_perc, "[ CPU %4s ]",  NULL },
-	{ ram_perc, "[ Mem %3s ]",  NULL },
-	{ datetime, "[ %s ]",       "%F %T" },
+    /* function     format          argument */
+    { wifi_perc,    icon_wln " %3s | ",    "wlp2s0" },
+    { cpu_perc,     icon_cpu " %4s | ",    NULL },
+    { ram_perc,     icon_mem " %3s | ",    NULL },
+    { entropy,      icon_ent " %s | ",     NULL },
+    { temp,         "%3s | ",              "/sys/class/thermal/thermal_zone0/temp" },
+    { battery_perc, icon_bat " %s | ",     "BAT1" },
+    { vol_perc,     icon_vol " %4s | ",    "default" },
+    { datetime,     "%s",                  "%a %b %d %I:%M %p" },
 };
